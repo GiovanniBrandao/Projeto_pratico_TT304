1417ceca381ddfce6aaacbd79431866d16128f1b
GiovanniBrandao <givirginiobrandao@gmail.com>
2024-10-29 15:50:20 -0300

    adicionando código corrigio, e mudança de valores nos arquivos 2 e 3

--- mergesort.c
+++ mergesort.c
@@ -2,60 +2,75 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
+#include <time.h>
 
 pthread_t t[8];
 
 typedef struct argumentos
 {
-    char *posidofile; // Nome do arquivo de entrada
-    char *outputfile; // Nome do arquivo de saída
-    int *valores;     // Armazenar os valores lidos dos arquivos
-    int tamanhoatual; // Quantidade de valores de um arquivo
+    char **posidofile; // Nome do arquivo de entrada (cada thread pode ter +1 arquivo)
+    int numdearqs;     // número de arquivos que determinada thread irá processar
+    char *outputfile;  // Nome do arquivo de saída
+    int *valores;      // Armazenar os valores lidos dos arquivos
+    int tamanhoatual;  // Quantidade de valores de um arquivo
+    int thread_id;     // identificador da thread
 
 } ThreadArg;
 
 void *ler_arquivo(void *arg)
 {
-
-    ThreadArg *args;
+    ThreadArg *argholder;
     int capacidade = 100; // Capacidade inicial do vetor
-    int valor;
+    int valor, i = 0;
+    double tempothread;
     FILE *arquivo;
+    struct timespec tempo_inicio, tempo_fim;
 
-    args = (ThreadArg *)arg;
+    clock_gettime(CLOCK_MONOTONIC, &tempo_inicio);
 
-    args->valores = malloc(capacidade * sizeof(int));
-    if (args->valores == NULL)
+    argholder = (ThreadArg *)arg;
+
+    argholder->valores = malloc(capacidade * sizeof(int));
+    if (argholder->valores == NULL)
     {
         printf("Erro ao alocar memória para valores\n");
         pthread_exit(NULL);
     }
 
-    arquivo = fopen(args->posidofile, "r");
-    if (arquivo == NULL)
+    argholder->tamanhoatual = 0;
+    for (int i = 0; i < argholder->numdearqs; i++)
     {
-        printf("Erro ao abrir arquivo\n");
-        pthread_exit(NULL);
-    }
-
-    args->tamanhoatual = 0;
-    while (fscanf(arquivo, "%d", &valor) == 1)
-    {
-        if (args->tamanhoatual >= capacidade)
+        arquivo = fopen(argholder->posidofile[i], "r");
+        if (arquivo == NULL)
         {
-            capacidade *= 2;
-            args->valores = realloc(args->valores, capacidade * sizeof(int));
-            if (args->valores == NULL)
-            {
-                printf("Erro ao realocar memória\n");
-                fclose(arquivo);
-                pthread_exit(NULL);
-            }
+            printf("Erro ao abrir arquivo %s\n", argholder->posidofile[i]);
+            pthread_exit(NULL);
         }
-        args->valores[args->tamanhoatual++] = valor;
+
+        // Lê e armazena valores do arquivo
+        while (fscanf(arquivo, "%d", &valor) == 1)
+        {
+            if (argholder->tamanhoatual >= capacidade)
+            {
+                capacidade *= 2;
+                argholder->valores = realloc(argholder->valores, capacidade * sizeof(int));
+                if (argholder->valores == NULL)
+                {
+                    printf("Erro ao realocar memória\n");
+                    fclose(arquivo);
+                    pthread_exit(NULL);
+                }
+            }
+            argholder->valores[argholder->tamanhoatual++] = valor;
+        }
+        fclose(arquivo);
     }
 
-    fclose(arquivo);
+    clock_gettime(CLOCK_MONOTONIC, &tempo_fim);
+
+    tempothread = (tempo_fim.tv_sec - tempo_inicio.tv_sec) + (tempo_fim.tv_nsec - tempo_inicio.tv_nsec) / 1e9;
+    printf("Thread %d: Tempo de execução = %f segundos\n", argholder->thread_id, tempothread);
+
     pthread_exit(NULL);
 }
 
@@ -72,7 +87,7 @@ int *mesclar_vetores(ThreadArg *args, int num_threads, int *tamanhototal)
         *tamanhototal += args[i].tamanhoatual;
     }
 
-    vetor_final = malloc(*tamanhototal * sizeof(int));
+    vetor_final = malloc((*tamanhototal) * sizeof(int));
     if (vetor_final == NULL)
     {
         printf("Erro ao alocar vetor final");
@@ -83,7 +98,7 @@ int *mesclar_vetores(ThreadArg *args, int num_threads, int *tamanhototal)
     {
         for (j = 0; j < args[i].tamanhoatual; j++)
         {
-            vetor_final[k++] = args[i].valores[j];
+            vetor_final[k++] = args[i].valores[j]; //valor j lido na posição da thread i
         }
     }
 
@@ -92,12 +107,15 @@ int *mesclar_vetores(ThreadArg *args, int num_threads, int *tamanhototal)
 
 void merge(int vet[], int esq, int meio, int dir)
 {
-    int tamVet1 = meio - esq + 1;
-    int tamVet2 = dir - meio;
+    int tamVet1, tamVet2, i, j, k;
+    int *E, *D;
+
+    tamVet1 = meio - esq + 1;
+    tamVet2 = dir - meio;
 
     // Alocação dinâmica para os vetores temporários
-    int *E = (int *)malloc(tamVet1 * sizeof(int));
-    int *D = (int *)malloc(tamVet2 * sizeof(int));
+    E = (int *)malloc(tamVet1 * sizeof(int));
+    D = (int *)malloc(tamVet2 * sizeof(int));
 
     if (E == NULL || D == NULL)
     {
@@ -106,13 +124,13 @@ void merge(int vet[], int esq, int meio, int dir)
     }
 
     // Copia os dados para os vetores temporários
-    for (int i = 0; i < tamVet1; i++)
+    for (i = 0; i < tamVet1; i++)
         E[i] = vet[esq + i];
 
-    for (int j = 0; j < tamVet2; j++)
+    for (j = 0; j < tamVet2; j++)
         D[j] = vet[meio + 1 + j];
 
-    int i = 0, j = 0, k = esq;
+    i = 0, j = 0, k = esq;
 
     // Combina os dois subarrays em ordem crescente
     while (i < tamVet1 && j < tamVet2)
@@ -155,7 +173,9 @@ void organizacao(int vet[], int esq, int dir)
 {
     if (esq < dir)
     {
-        int meio = esq + (dir - esq) / 2;
+        int meio;
+
+        meio = esq + (dir - esq) / 2;
 
         // Ordena a primeira e a segunda metade
         organizacao(vet, esq, meio);
@@ -168,13 +188,18 @@ void organizacao(int vet[], int esq, int dir)
 
 int main(int arqc, char *arqv[])
 {
-    int qnt, i, tamanhototal, numarqs;
+    int qnt, i, j, tamanhototal, numarqs = 0;
+    int arqsporthread, arqsextras, posiatualdoarq = 2;
     int *vetorfinal;
     char *outputfile = NULL;
+    double tempoprogtot;
     ThreadArg *arg;
     FILE *output;
+    struct timespec inicioprog, fimprog;
 
-    qnt = atoi(arqv[1]);
+    clock_gettime(CLOCK_MONOTONIC, &inicioprog);
+
+    qnt = atoi(arqv[1]); //quantidade de threads
 
     // Identificar o arquivo de saída na linha de comando
     for (i = 2; i < arqc; i++)
@@ -192,18 +217,53 @@ int main(int arqc, char *arqv[])
         return 1;
     }
 
-    numarqs = arqc - 2;
+// conta quantos arquivo tem no vetor
+    for (i = 2; i < arqc; i++)
+    {
+        if (strcmp(arqv[i], "-o") == 0)
+        {
+            break;
+        }
+        numarqs++;
+    }
 
-    arg = malloc(numarqs * sizeof(ThreadArg));
+    if (numarqs == 0)
+    {
+        printf("Erro: Nenhum arquivo de entrada especificado.\n");
+        return 1;
+    }
+
+    if (qnt > numarqs)
+    {
+        printf("Erro: O número de threads não pode ser maior que o número de arquivos.\n");
+        return 1;
+    }
+
+    arg = malloc(numarqs * sizeof(ThreadArg)); //alocação da struct para passar valores
+
+    arqsporthread = numarqs / qnt; //numero de arquivos por thread
+    arqsextras = numarqs % qnt; //caso os arquivos não sejam divididos igualmente
+
+    for (i = 0; i < numarqs; i++)
+    {
+        arg[i].outputfile = outputfile;
+        arg[i].valores = NULL;
+        arg[i].tamanhoatual = 0;
+        arg[i].numdearqs = arqsporthread + (i < arqsextras ? 1 : 0); //atribui os arquivos extras caso aquela thread não tenha
+        printf("quantidade de arquivos: %d\n", arg[i].numdearqs);
+
+        arg[i].posidofile = malloc(arg[i].numdearqs * sizeof(char *)); // Aloca memória para os nomes dos arquivos
+        for (int j = 0; j < arg[i].numdearqs; j++)
+        {
+            arg[i].posidofile[j] = arqv[posiatualdoarq++]; // Atribui os nomes dos arquivos
+        }
+    }
 
     for (i = 0; i < qnt; i++)
     {
-        arg[i].posidofile = arqv[i + 2]; 
-        arg[i].outputfile = outputfile;  
-        arg[i].valores = NULL;           
-        arg[i].tamanhoatual = 0;         
+        arg[i].thread_id = i + 1; //apenas para visualizar qual thread foi executada
 
-        if (pthread_create(&t[i], NULL, ler_arquivo, (void *)&arg[i]) != 0) //cria cada thread
+        if (pthread_create(&t[i], NULL, ler_arquivo, (void *)&arg[i]) != 0) // cria cada thread
         {
             printf("Erro ao criar a thread %d\n", i + 1);
             free(arg);
@@ -230,19 +290,7 @@ int main(int arqc, char *arqv[])
     }
     for (int i = 0; i < tamanhototal; i++)
     {
-        fprintf(output, "%d ", vetorfinal[i]);
-
-        // Se 5 números já foram escritos, pula para a próxima linha
-        if ((i + 1) % 5 == 0)
-        {
-            fprintf(output, "\n"); // Nova linha
-        }
-    }
-
-        // Adiciona uma nova linha no final, se não tiver terminado em uma nova linha
-    if (tamanhototal % 5 != 0)
-    {
-        fprintf(output, "\n");
+        fprintf(output, "%d\n", vetorfinal[i]);
     }
 
     fclose(output);
@@ -252,8 +300,16 @@ int main(int arqc, char *arqv[])
     for (i = 0; i < qnt; i++)
     {
         free(arg[i].valores);
+            free(arg[i].posidofile);
+
     }
     free(arg);
+
+    clock_gettime(CLOCK_MONOTONIC, &fimprog);
+
+    tempoprogtot = (fimprog.tv_sec - inicioprog.tv_sec) + (fimprog.tv_nsec - inicioprog.tv_nsec) / 1e9;
+
+    printf("Tempo total do programa: %.6f segundos\n", tempoprogtot);
 
     return 0;
 }
